012cc15ac3c6ea355981c5a32648953faa27598e
이현우 <ihyeon-u@ihyeon-uui-MacBookPro.local>
2021-01-17 22:06:59 +0900

    SearchReduplicationData.java

---
+++ Algorithm/src/SearchReduplicationData.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+/* 배열에서 중복 데이터 찾기 */
+public class SearchReduplicationData {
+	
+	public void solution(String[] datas) {
+		HashMap<String, Integer> hm = new HashMap<>();
+		
+		for(String data : datas) {
+			hm.put(data, hm.getOrDefault(data,0) + 1);
+		}
+		
+		Stream<HashMap.Entry<String, Integer>> stream = hm.entrySet().stream().filter(s -> s.getValue() > 1);
+		stream.forEach(m -> System.out.println(m.getKey()));
+	}
+	
+	public static void main(String[] args) {
+		SearchReduplicationData searchReduplicationData = new SearchReduplicationData();
+		String[] data = {"mislav", "stanko", "mislav", "ana", "stanko"}; 
+		
+		searchReduplicationData.solution(data);
+	}
+
+}
